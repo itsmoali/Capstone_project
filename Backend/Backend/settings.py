ef0c3a92c646387f7268e25f68a23bfb163ec8f8
moali <moali@uni.minerva.edu>
2023-10-04 11:40:40 +0800

    Signup Impleneted

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -20,20 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--gqn8cxao&q&%n%68f56)_2=isyb@bfngt^*%=9_$)(q97%dg2'
+SECRET_KEY = 'django-insecure--gqn8cxao&q&%n%68f56)_2=isyb@bfngt^%=9_$)(q97%dg2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
 
-CORES_ALLOWED_ORIGINS = [
-    # "http://localhost:3000",
-    # "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",]
 
-CORS_ALLOWED_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
+
+
 
 
 # Application definition
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Backend.urls'
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -15,53 +15,71 @@ import Container from '@mui/material/Container';
 import Alert from '@mui/material/Alert';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { FormControl, InputLabel } from '@mui/material';
- 
+import axios from 'axios';
+
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+
+
+
 const Signup = () => {
     const navigate = useNavigate();
  
+    const [currentUser, setCurrentUser] = useState();
     const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('');
+    const [error, setError] = useState('');
     const [password, setPassword] = useState('');
     const [confirm_pwd, setConfirm_pwd] = useState('');
-    // const [passwordsMatch, setPasswordsMatch] = useState(true);
- 
-    // const onSubmit = async (e) => {
-    //   e.preventDefault()
 
-    //     if (password !== confirm_pwd) {
-    //         return alert("Passwords donot matche. Please try again")
-    //         // setPasswordsMatch(true);
-    //     }
-        
-      
 
-    //   await createUserWithEmailAndPassword(auth, email, password)
-    //     .then((userCredential) => {
 
-    //         const user = userCredential.user;
-    //         console.log(user);
-    //         navigate("/login")
-  
-    //     })
-    //     .catch((error) => {
-    //         const errorCode = error.code;
-    //         const errorMessage = error.message;
-    //         console.log(errorCode, errorMessage);
-    
-    //     });
- 
-   
-    // }
- 
+function handleSubmit(e) {
+    e.preventDefault();
+    if (password !== confirm_pwd) {
+        setError("Passwords do not match");
+        return;
+    }
+    if (password.length < 8) {
+        setError("Password must be at least 8 characters");
+        return;
+    }
+    if (username.length < 3) {
+        setError("Username must be at least 3 characters");
+        return;
+    }
+    if (email == ''){
+        setError("Please enter an Email");
+        return;
+    }
+
+    axios.post('/register', {
+            
+            email: email,
+            username: username,
+            password: password
+        })
+        .then((response) => {
+            // Handle successful response
+            console.log('Registration successful', response.data);
+        })
+        .catch((error) => {
+            // Handle network error
+            console.error('Network error:', error.response.data);
+            // You can set an error state or display an error message here
+        });
+}
+
+
+
   return (
     
-        
-        
 
         <Container component="main" maxWidth="xs">
-            {console.log(confirm_pwd)}
-            {console.log(password)}
-            
-            
+
             {/* <CssBaseline /> */}
                 <Box
                 sx={{
@@ -72,15 +90,12 @@ const Signup = () => {
                 }}
                 >
 
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
                 <Typography component="h1" variant="h5">
                     Sign up
                     
                 </Typography>
-                {/* {error && <Alert severity="error">{error}</Alert>}
-                {currentUser && <Alert severity="success">You have successfully signed up</Alert>} */}
+                {error && <Alert severity="error">{error}</Alert>}
+                {currentUser && <Alert severity="success">You have successfully signed up</Alert>}
                 
                 
                 <Box component="form"  sx={{ mt: 3 }}>
@@ -88,72 +103,58 @@ const Signup = () => {
                     {/* <FormControl action=""> */}
                     
                     <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                        autoComplete="given-name"
-                        name="firstName"
-                        required
-                        fullWidth
-                        id="firstName"
-                        label="First Name"
-                        autoFocus
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField 
-                        required
-                        fullWidth
-                        id="lastName"
-                        label="Last Name"
-                        name="lastName"
-                        autoComplete="family-name"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField 
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        /> 
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField 
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="new-password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField  
-                        required
-                        fullWidth
-                        name="confirm-password"
-                        label="Confirm-Password"
-                        type="password"
-                        id="confirm-password"
-                        autoComplete="new-password"
-                        onChange = {(e) => setConfirm_pwd(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControlLabel
-                        control={<Checkbox value="allowExtraEmails" color="primary" />}
-                        label="I want to receive inspiration, marketing promotions and updates via email."
-                        />
-                    </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField 
+                            // required
+                            fullWidth
+                            id="username"
+                            label="username"
+                            name="username"
+                            autoComplete="family-name"
+                            onChange = {(e) => setUsername(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField 
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            /> 
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField 
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="new-password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField  
+                            required
+                            fullWidth
+                            name="confirm-password"
+                            label="Confirm-Password"
+                            type="password"
+                            id="confirm-password"
+                            autoComplete="new-password"
+                            onChange = {(e) => setConfirm_pwd(e.target.value)}
+                            />
+                        </Grid>
                     </Grid>
                     <Button
+
+                    onClick={handleSubmit}
                     type="submit"
-                    
                     fullWidth
                     variant="contained"
                     sx={{ mt: 3, mb: 2 }}
