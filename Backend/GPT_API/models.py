6ace09d208a36a63c4f7ed72bff9abdfde6cc3c6
moali <moali@uni.minerva.edu>
2024-02-24 17:49:46 +0000

    Model CHanged

--- Backend/GPT_API/models.py
+++ Backend/GPT_API/models.py
@@ -7,11 +7,7 @@ class Courses(models.Model):
     course_name = models.CharField(max_length=100, unique=True) 
     course_difficulty = models.CharField(max_length=100, default="Beginner")
     course_duration = models.CharField(max_length=100, default="1")
-<<<<<<< HEAD
-    course_schedule = models.JSONField(default=dict)
-=======
     course_details = models.JSONField(default=dict)
->>>>>>> Main-Front-end
 
     
     def __repr__(self):
