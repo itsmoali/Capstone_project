9422d75554b3f55e19aab76e19aa8577fbee9e5a
moali <moali@uni.minerva.edu>
2024-02-24 19:58:29 +0000

    Dtabase changed reverted

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 
 }
 
-DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
