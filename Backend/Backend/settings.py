7f9b298a5ac8b243fe19aec70c2e40a9a31435b9
moali <moali@uni.minerva.edu>
2024-03-05 08:41:07 +0000

    Local Host changed

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--gqn8cxao&q&%n%68f56)_2=isyb@bfngt^%=9_$)(q97%dg2'
 DEBUG = os.environ.get('DEBUG', 'True') =="True"
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['local','127.0.0.1']
 
 
 
